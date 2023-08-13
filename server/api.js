5ff09055334a5aa088efde739e051d81390aa611
Chris Mastris <cmastris@hotmail.com>
2023-08-13 18:29:43 +0100

    Add `minionId` param middleware

--- server/api.js
+++ server/api.js
@@ -3,9 +3,25 @@ const db = require('./db');
 
 const apiRouter = express.Router();
 
+// Helper Functions
+
 const isDefined = data => {
   return typeof data !== "undefined";
 }
+
+// Minions Middleware
+
+apiRouter.param('minionId', (req, res, next, id) => {
+  const minion = db.getFromDatabaseById('minions', String(id));
+  if (minion) {
+    req.minion = minion;
+    next();
+  } else {
+    res.status(404).send(`A minion with the ID of '${id}' was not found.`);
+  }
+});
+
+// Minions Routes
 
 apiRouter.get('/minions', (req, res) => {
   res.send(db.getAllFromDatabase('minions'));
