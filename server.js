31a0253752c8089df0bc66fee1ee85253327415c
Chris Mastris <cmastris@hotmail.com>
2023-08-13 15:57:59 +0100

    Start server listening

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ const apiRouter = require('./server/api');
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => console.log(`Server started; listening on port ${PORT}.`));
 }
