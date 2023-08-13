a944e662a6f4b32c2db2ec8e7d0d539b86b7e8bc
Chris Mastris <cmastris@hotmail.com>
2023-08-13 17:03:58 +0100

    Add GET /minions route

--- server/api.js
+++ server/api.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const db = require('./db');
+
 const apiRouter = express.Router();
 
+apiRouter.get('/minions', (req, res) => {
+  res.send(db.getAllFromDatabase('minions'));
+});
 
 
 module.exports = apiRouter;
