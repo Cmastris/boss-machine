b3ab30942ffa7cef3ac7954989bd007d2bf2c5d3
Chris Mastris <cmastris@hotmail.com>
2023-08-13 18:09:34 +0100

    Add POST /minions route

--- server/api.js
+++ server/api.js
@@ -3,8 +3,24 @@ const db = require('./db');
 
 const apiRouter = express.Router();
 
+const isDefined = data => {
+  return typeof data !== "undefined";
+}
+
 apiRouter.get('/minions', (req, res) => {
   res.send(db.getAllFromDatabase('minions'));
+});
+
+apiRouter.post('/minions', (req, res) => {
+  const { name, title } = req.body;
+  const salary = Number(req.body.salary);
+  
+  if (isDefined(name) && isDefined(title) && isDefined(salary)) {
+    const newMinion = db.addToDatabase('minions', { name, title, salary });
+    res.status(201).send(newMinion);
+  } else {
+    res.status(400).send('Missing data in request body.');
+  }
 });
 
 
